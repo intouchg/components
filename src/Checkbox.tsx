b277767d627bad14b86d8964c948a9301738704e
Cody Persinger <cody.persinger@intouchsol.com>
2020-10-13 15:27:50 -0500

    First pass on Checkbox component, bump to v0.0.25

---
+++ src/Checkbox.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from 'styled-components'
+import { styleFunctions } from './core'
+import type { StyleProps } from './core'
+
+const CheckboxContainer = styled.div<StyleProps>`
+    display: inline-block;
+    vertical-align: middle;
+    cursor: pointer;
+`
+
+const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
+    border: 0;
+    clip: rect(0 0 0 0);
+    clippath: inset(50%);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    white-space: nowrap;
+    width: 1px;
+`
+
+const BaseCheckbox = ({
+	id,
+	className,
+	checked,
+	onClick,
+	children,
+}: {
+    id?: string
+    className?: string
+    checked: boolean
+    onClick: (checked: boolean) => void
+    children: React.ReactNode
+}) => (
+	<CheckboxContainer
+		className={className}
+		onClick={() => onClick(!checked)}
+	>
+		<HiddenCheckbox
+			id={id}
+			checked={checked}
+		/>
+		{children}
+	</CheckboxContainer>
+)
+
+const Checkbox = styled(BaseCheckbox)<StyleProps>`
+    box-sizing: border-box;
+    ${styleFunctions}
+`
+
+Checkbox.defaultProps = {}
+
+Checkbox.displayName = 'Checkbox'
+
+export { Checkbox }
