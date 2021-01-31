f5dd127d5dfb926a69cfe5e93b9d459be034ae2a
Cody Persinger <cody.persinger@cpersinger-mac.local>
2021-01-30 23:38:41 -0600

    Update Textarea styles, export Textarea, bump to v0.0.81

--- src/Textarea.tsx
+++ src/Textarea.tsx
@@ -4,8 +4,28 @@ import { defaultVariantName } from '@i/theme'
 import { styleFunctions, variantsFunction, sx } from './core'
 import type { StyleProps, VariantProps } from './core'
 
-const Textarea = styled.textarea<StyleProps & VariantProps>`
+const Textarea = styled.textarea<
+	& StyleProps
+	& VariantProps
+	& { resize?: string }
+>`
 	box-sizing: border-box;
+	resize: ${(props) => props.resize || 'none'};
+	padding: 0 0.25em;
+	font-family: inherit;
+    font-size: inherit;
+    line-height: inherit;
+    color: inherit;
+	border-width: 1px;
+	border-style: solid;
+	border-radius: 2px;
+	outline: none;
+
+	&:focus-visible {
+		outline: 2px auto #005FD7;
+        outline: 2px auto -webkit-focus-ring-color;
+	}
+
 	${variantsFunction('textareas')}
 	${styleFunctions}
 	${sx}
