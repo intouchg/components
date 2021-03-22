d798198bdd1bb59e9f78c50efa034398eead5c96
Cody Persinger <cody.persinger@cpersinger-mac.lan1>
2021-03-22 11:21:42 -0500

    Added Paragraph component, bump to v0.0.103

---
+++ src/Paragraph.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+import { defaultVariantName } from '@i/theme'
+import { styleFunctions, variantsFunction, sx } from './core'
+import type { StyleProps, VariantProps, SXProps } from './core'
+
+const Paragraph = styled.p<
+	& StyleProps
+	& VariantProps
+	& SXProps
+>`
+	box-sizing: border-box;
+	${variantsFunction('paragraphs')}
+	${styleFunctions}
+	${sx}
+`
+
+Paragraph.defaultProps = { variant: defaultVariantName }
+
+;(Text as any).themeComponent = 'paragraph'
+
+Paragraph.displayName = 'Paragraph'
+
+export { Paragraph }
